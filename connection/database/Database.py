2d41fa6d68b87b627ced2faafaf1deba5663d679
AdirtKa <AdirtKa@yandex.ru>
2024-04-01 14:58:14 +1000

    wrap database to another class

--- connection/database/Database.py
+++ connection/database/Database.py
@@ -1,5 +1,46 @@
-class Database(object):
-    def __init__(self):
-        self.org = ""
-        self.url = ""
-        self.token = ""
+from datetime import datetime, timedelta
+import time
+
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+
+class Database():
+    def __init__(self, organization: str, url: str, token: str):
+        self.__org = organization
+        # self.__url = url
+        self.__token = token
+        self.current_bucket = ""
+
+        self.__client = InfluxDBClient(org=organization, url=url, token=token)
+
+    def write(self, measurements: list[tuple[str, float]], timestamp: datetime = None):
+        if self.current_bucket == "":
+            raise ValueError("bucket  isn't specified")
+
+        write_api = self.__client.write_api(write_options=SYNCHRONOUS)
+        point = Point("environment")
+
+        if timestamp is not None:
+            point.time(timestamp)
+
+        for measurement_name, measurement in measurements:
+            point.field(measurement_name, measurement)
+
+        write_api.write(bucket=self.current_bucket, record=point, write_precision="ms")
+
+        print(point.to_line_protocol(), "is written")
+
+    def select(self, end_time: datetime, minutes_delta: float):
+        start_time = end_time - timedelta(minutes=minutes_delta)
+
+        start_time_format = int(time.mktime(start_time.timetuple()))
+        api = self.__client.query_api()
+        query = f"""from(bucket: "{self.current_bucket}")
+                    |> range(start: {start_time_format})
+                    |> filter(fn: (r) => r._measurement == "environment")"""
+        tables = api.query(query, org=self.__org)
+
+        for table in tables:
+            for record in table.records:
+                print(record)
--- connection/database/main.py
+++ connection/database/main.py
@@ -1,43 +1,41 @@
+import datetime
 import os
 import time
 
-from Sensors.sensors import HumiditySensor, PressureSensor, TempSensor
+from FakeSensors.sensors import HumiditySensor, PressureSensor, TempSensor
+from Database import Database
 
-from influxdb_client import InfluxDBClient, Point
-from influxdb_client.client.write_api import SYNCHRONOUS
+from dotenv import load_dotenv
 
 
 def main():
     """Entry point."""
+    load_dotenv()
     token = os.environ.get('INFLUXDB_TOKEN')
-    org = 'apogei'
-    url = 'http://localhost:8086'
+    org = os.environ.get('ORGANIZATION')
+    url = os.environ.get('URL')
+
+    db = Database(token=token, organization=org, url=url)
+    db.current_bucket = 'sensors'
 
     temp = TempSensor()
     pressure = PressureSensor()
     humidity = HumiditySensor()
 
-    client = InfluxDBClient(url=url, org=org, token=token)
+    # for _ in range(1000):
+    #     temp_val = temp.value
+    #     pressure_val = pressure.value
+    #     humidity_val = humidity.value
+    #
+    #     measurements = [('Temperature', temp_val),
+    #                     ('Pressure', pressure_val),
+    #                     ('Humidity', humidity_val)]
+    #
+    #     db.write(measurements=measurements)
+    #
+    #     time.sleep(1)
 
-    bucket = 'sensors'
-
-    write_api = client.write_api(write_options=SYNCHRONOUS)
-
-    for _ in range(1000):
-        temp_val = temp.value
-        pressure_val = pressure.value
-        humidity_val = humidity.value
-
-        point = (
-            Point('environment')
-            .field('Temperature', temp_val)
-            .field('Pressure', pressure_val)
-            .field('Humidity', humidity_val)
-        )
-
-        print(f'Writing {point.to_line_protocol()}')
-        write_api.write(bucket=bucket, org=org, record=point)
-        time.sleep(1)
+    db.select(end_time=datetime.datetime.now(), minutes_delta=30)
 
 
 if __name__ == '__main__':
