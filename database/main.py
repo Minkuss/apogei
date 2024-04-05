463304adb5d570ac3742baec223bc29a8246563c
HonFi <74440672+Honfi555@users.noreply.github.com>
2024-04-05 13:12:13 +1000

    add select queries
    add select all and select by timestamp range

--- database/main.py
+++ database/main.py
@@ -11,12 +11,15 @@ def main() -> None:
 
     db = Database()
 
-    for i in range(5):
-        values = [datetime.datetime.now()] + [sensor.value for sensor in sensors]
-        db.insert(values)
-        time.sleep(1)
+    # for i in range(5):
+    #     values = [datetime.datetime.now()] + [sensor.value for sensor in sensors]
+    #     db.insert(values)
+    #     time.sleep(1)
+
+    result = db.select_by_timestamp_range(datetime.datetime.now(), 20)
+    for row in result:
+        print(row)
 
 
 if __name__ == '__main__':
-    load_dotenv()
     main()
