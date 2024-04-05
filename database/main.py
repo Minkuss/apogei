bf6e4f8720a0577de35e72367af15bf0f48b7bcb
HonFi <74440672+Honfi555@users.noreply.github.com>
2024-04-05 12:19:41 +1000

    delete unused imports

--- database/main.py
+++ database/main.py
@@ -1,17 +1,15 @@
 import datetime
-import os
 import time
 
 from Database import Database
 from Sensors.Sensors import HumiditySensor, PressureSensor, Sensor, TempSensor
-from dotenv import load_dotenv
 
 
 def main() -> None:
     """Entry point."""
     sensors: list[Sensor] = [HumiditySensor(), TempSensor(), PressureSensor()]
 
-    db = Database(os.getenv('DATABASE_PASSWORD'))
+    db = Database()
 
     for i in range(5):
         values = [datetime.datetime.now()] + [sensor.value for sensor in sensors]
