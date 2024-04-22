a90789002bcfc7144873b3fec3f90f3232d18135
AdirtKa <AdirtKa@yandex.ru>
2024-04-22 11:40:58 +1000

    receive data in json format

--- connection/client/client.py
+++ connection/client/client.py
@@ -10,8 +10,8 @@ import pickle
 
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 
-SERVER_HOST = '10.192.2.164'
-SERVER_PORT = 12345
+SERVER_HOST = '192.168.0.8'
+SERVER_PORT = 30034
 
 p = 0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AACAA68FFFFFFFFFFFFFFFF
 g = 2
@@ -88,7 +88,8 @@ def decrypt_message(encrypted_message: bytes, key: bytes) -> bytes:
     decrypted_data = decryptor.update(ciphertext) + decryptor.finalize()
 
     # Удаляем дополнение
-    unpadded_data = unpadder.update(decrypted_data) + unpadder.finalize()
+    unpadded_data = (unpadder.update(decrypted_data)
+                      + unpadder.finalize())
 
     return unpadded_data
 
@@ -97,13 +98,11 @@ def handle_server(conn: socket) -> None:
     """Handle server connection."""
     shared_key = perform_key_exchange(conn)
 
-    while True:
-        encrypted_message = conn.recv(1024)
-        if not encrypted_message:
-            break
-        decrypted_message = decrypt_message(encrypted_message, shared_key)
-        # data = pickle.loads(decrypted_message)
-        print('Received message:', json.loads(decrypted_message.decode(encoding='utf-8', errors='replace')))
+    encrypted_message = conn.recv(2 ** 16)
+    decrypted_message = decrypt_message(encrypted_message, shared_key).decode(encoding='utf-8')
+
+    print(decrypted_message + '\n')
+    print(json.loads(decrypted_message))
 
 
 def main() -> None:
