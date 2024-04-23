de2ff210577777713d8dcc15a6a5d665549b16d9
Daniil <AdirtKa@yandex.ru>
2024-04-23 12:47:57 +1000

    fix import base sensor from another file

--- database/Sensors/Sensors.py
+++ database/Sensors/Sensors.py
@@ -1,32 +1,31 @@
 import random
 from random import uniform
 
-from abc import ABC, abstractmethod
+from database.Sensors.BaseSensor import Sensor
 
-
-class Sensor(ABC, object):
-    """Representation of a sensor with a fictive data."""
-
-    def __init__(self) -> None:
-        """Initialize variables."""
-        self.__value: float = 0
-
-    @abstractmethod
-    def diff(self) -> float:
-        """Difference between current value and new value."""
-        pass
-
-    @property
-    def value(self) -> float:
-        """Getter for the value of the sensor."""
-        value = self.__value
-        self.__value += self.diff()
-        return value
-
-    @value.setter
-    def value(self, value) -> None:
-        """Setter for the value of the sensor."""
-        self.__value = value
+# class Sensor(ABC, object):
+#     """Representation of a sensor with a fictive data."""
+#
+#     def __init__(self) -> None:
+#         """Initialize variables."""
+#         self.__value: float = 0
+#
+#     @abstractmethod
+#     def diff(self) -> float:
+#         """Difference between current value and new value."""
+#         pass
+#
+#     @property
+#     def value(self) -> float:
+#         """Getter for the value of the sensor."""
+#         value = self.__value
+#         self.__value += self.diff()
+#         return value
+#
+#     @value.setter
+#     def value(self, value) -> None:
+#         """Setter for the value of the sensor."""
+#         self.__value = value
 
 
 class TempSensor(Sensor):
